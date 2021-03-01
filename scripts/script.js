3b910b2bfe9e994d327fb15e4f6188d132a90dea
Nite <solitude.loner7@gmail.com>
2021-03-01 22:30:40 +0700

    [Feature] Filter todo elements with either active or completed state && Added ID to each todo items

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,32 @@
 (() => {
     const todos = [
         {
+            id: 1,
             title: "Complete online Javascript course",
             status: true,
         },
         {
+            id: 2,
             title: "Jog around the park 3x",
             status: false,
         }, 
         {
+            id: 3,
             title: "10 minutes meditation",
             status: false,
         },
         {
+            id: 4,
             title: "Read for 1 hour",
             status: false,
         },
         {
+            id: 5,
             title: "Pick up groceries",
             status: false,
         },
         {
+            id: 6,
             title: "Complete Todo App on Frontend Mentor",
             status: false,
         }
@@ -36,6 +42,7 @@
 
     const filterActiveTodoElement = document.querySelector(".js-filter-active");
     const filterCompletedTodoElement = document.querySelector(".js-filter-completed");
+    const filterAllTodoElement = document.querySelector(".js-filter-all");
 
     // todoFormElement.addEventListener("submit", (event) => {
     //     event.preventDefault();
@@ -64,9 +71,16 @@
         renderTodos(filteredTodos);
     })
 
+    filterAllTodoElement.addEventListener("click", (event) => {
+        filteredTodos = [...todos];
+
+        clearTodoElements();
+        renderTodos(filteredTodos);
+    })
+
     function renderTodos(todos) {
-        todos.forEach((todo, index) => {
-            todoListElement.appendChild(createTodoElement(todo, index));
+        todos.forEach((todo) => {
+            todoListElement.appendChild(createTodoElement(todo));
         })
     }
 
@@ -74,7 +88,7 @@
         todoListElement.textContent = "";
     }
 
-    function createTodoElement(todo, index) {
+    function createTodoElement(todo) {
         var todoItemElement = document.createElement("li");
         var todoFormElement = document.createElement("form");
         var todoShowElementContainer = document.createElement("div");
@@ -95,7 +109,7 @@
 
         todoCheckBoxElement.type = "checkbox";
 
-        todoCheckBoxElement.id = `checkbox${index}`;
+        todoCheckBoxElement.id = `checkbox${todo.id}`;
         todoCheckBoxElement.checked = todo.status;
 
         todoLabelElement.setAttribute("for", todoCheckBoxElement.id);
